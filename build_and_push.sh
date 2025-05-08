0a0c15ad39acb0e364d4677fae2e98809ddb7a2a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2025-05-08 22:02:49 +0200

    jdk

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,7 +5,7 @@ docker build -t mapreduce/javamapreduce -f Dockerfile.server .
 docker build -t mapreduce/javamapreduce-cli -f Dockerfile.cli .
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v4e3t3o3
-docker tag mapreduce/javamapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:latest
+docker tag mapreduce/javamapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:jdk
 docker tag mapreduce/javamapreduce-cli:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce-cli:latest
-docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:latest
+docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:jdk
 docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce-cli:latest
