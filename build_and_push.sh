09b9501184132e7d5911898e11ab5c1a53a5aac3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2025-05-10 19:06:34 +0200

    err-det3

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,7 +5,7 @@ docker build -t mapreduce/javamapreduce -f Dockerfile.server .
 docker build -t mapreduce/javamapreduce-cli -f Dockerfile.cli .
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v4e3t3o3
-docker tag mapreduce/javamapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:err-det
+docker tag mapreduce/javamapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:err-det3
 docker tag mapreduce/javamapreduce-cli:latest public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce-cli:latest
-docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:err-det
+docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce:err-det3
 docker push public.ecr.aws/v4e3t3o3/mapreduce/javamapreduce-cli:latest
